823d158bc6106c837ed90d6914f142a7d5eb5877
DPintoLL <diogosmp@gmail.com>
2020-09-15 00:13:48 -0700

    Basic socket config on the server, cleanup of default React app

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import io from "socket.io-client";
+import Peer from "simple-peer";
+import style from "styled-components";
 
 function App() {
+  const [yourID, setYourID] = useState("");
+  const [users, setUsers] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 
--- server.js
+++ server.js
@@ -5,6 +5,33 @@ const io = require("socket.io")(server);
 const PORT = 8000;
 const moment = require("moment");
 
-const users = {};
+const users = {
+  list: ["Bob", "Olivia", "Rick", "Julia"],
+  count: 0,
+  setNickName: function (id) {
+    this[id] = this[id] || this.list[this.count];
+    this.count = ++this.count % this.list.length;
+    return this[id];
+  }
+};
+
+// Listening for a "connection" event
+io.on("connection", socket => {
+  const nickName = users.setNickName(socket.id);
+  socket.emit("nickname", nickName);
+
+  socket.on("send message", body => {
+    io.emit("message", body);
+  });
+
+  // Inform all users when a new user connects and disconnect
+  io.emit("conn message", `User ${nickName} connected`);
+  socket.on("disconnect", () => {
+    io.emit("conn message", `User ${nickName} disconnected`);
+    console.log(`User ${nickName} disconnected`);
+  });
+
+  console.log(`${moment()} - User ${socket.id} connected`);
+});
 
 server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
